fa66b88b96893352775155bbcf494642c08f0c70
DemonVenom666 <demonvenom9@gmail.com>
2025-03-20 21:36:57 +0100

    styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx';
+import s from './Profile.module.css';
+
 const Profile = ({
   image,
   name,
@@ -6,23 +9,23 @@ const Profile = ({
   stats: { followers, views, likes },
 }) => {
   return (
-    <div>
+    <div className={s.box}>
       <div>
-        <img src={image} alt={name} />
+        <img className={s.img} src={image} alt={name} />
         <p>{name}</p>
         <p>@{tag}</p>
         <p>{location}</p>
       </div>
-      <ul>
-        <li>
+      <ul className={s.list}>
+        <li className={s.item}>
           <span>Followers</span>
           <span>{followers}</span>
         </li>
-        <li>
+        <li className={clsx(s.item, s.item_border)}>
           <span>Views</span>
           <span>{views}</span>
         </li>
-        <li>
+        <li className={s.item}>
           <span>Likes</span>
           <span>{likes}</span>
         </li>
